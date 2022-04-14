6360cd8a8a4d01a63a776604cf8ecb3c84c1d768
Robby Bush <crobertbush@gmail.com>
2022-04-13 23:36:24 -0400

    update

---
+++ app.js
@@ -0,0 +1,35 @@
+var a = 10;
+var b = 20;
+var c = 30;
+var d = 40;
+var e = 50;
+var f = 60;
+var g = 70;
+var h = 80;
+var i = 90;
+var j = 100;
+
+
+//addition
+console.log(a+b);
+console.log(j+f);
+
+//subtraction
+console.log(j-b);
+console.log(g-f);
+
+//multiplication
+console.log(a*c);
+console.log(j*h);
+
+//division
+console.log(i/d);
+console.log(h/f);
+
+//increments
+console.log(g++);
+console.log(e++);
+
+//decrements
+console.log(i--);
+console.log(g--);
